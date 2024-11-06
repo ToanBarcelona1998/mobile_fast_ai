701dd609ff70a60e05cad270b603ac6862a274df
Nguyen Van Toan <toannv@192.168.68.117>
2024-11-06 15:48:18 +0700

    Add: User repository

--- cores/domain/lib/src/repository/user_repository.dart
+++ cores/domain/lib/src/repository/user_repository.dart
@@ -1,3 +1,9 @@
-abstract interface class UserRepository {
+import 'package:domain/src/entity/entity.dart';
 
+abstract interface class UserRepository {
+  Future<User> get();
+
+  Future<bool> update({
+    required UpdateUserRequest request,
+  });
 }
