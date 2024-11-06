cd1aab61dfe85f75f63b1aed8356a035e99bd835
Nguyen Van Toan <toannv@192.168.68.117>
2024-11-06 13:58:11 +0700

    Add: Entity

---
+++ cores/domain/lib/src/entity/requests/register_request.dart
@@ -0,0 +1,9 @@
+final class RegisterRequest {
+  final String userName;
+  final String password;
+
+  const RegisterRequest({
+    required this.userName,
+    required this.password,
+  });
+}
---
+++ cores/domain/lib/src/entity/user_credit.dart
@@ -0,0 +1,9 @@
+final class UserCredit {
+  final int id;
+  final int remaining;
+
+  const UserCredit({
+    required this.id,
+    required this.remaining,
+  });
+}
