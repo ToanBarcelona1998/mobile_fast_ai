7375be5ebd3894e5afb945c5b4c67c398484ce6b
Nguyen Van Toan <toannv@192.168.100.15.non-exists.ptr.local>
2024-11-07 10:10:06 +0700

    Add: Register user service dependency

--- lib/config/di.dart
+++ lib/config/di.dart
@@ -3,6 +3,7 @@ import 'package:dio/dio.dart';
 import 'package:domain/domain.dart';
 import 'package:get_it/get_it.dart';
 import 'package:mobile_fast_ai/src/application/data/service/auth/auth_service_impl.dart';
+import 'package:mobile_fast_ai/src/application/data/service/user/user_service_impl.dart';
 import 'fast_ai_config.dart';
 
 final GetIt getIt = GetIt.instance;
@@ -32,6 +33,12 @@ Future<void> initDependency(FastAIConfig config) async {
     ),
   );
 
+  getIt.registerLazySingleton<UserServiceGenerator>(
+        () => UserServiceGenerator(
+      getIt.get<Dio>(),
+    ),
+  );
+
   // Service
   getIt.registerLazySingleton<AuthService>(
     () => AuthServiceImpl(
@@ -39,8 +46,18 @@ Future<void> initDependency(FastAIConfig config) async {
     ),
   );
 
+  getIt.registerLazySingleton<UserService>(
+        () => UserServiceImpl(
+      getIt.get<UserServiceGenerator>(),
+    ),
+  );
+
   // Repository
   getIt.registerLazySingleton<AuthRepository>(
     () => AuthRepositoryImpl(getIt.get<AuthService>()),
+  );
+
+  getIt.registerLazySingleton<UserRepository>(
+        () => UserRepositoryImpl(getIt.get<UserService>()),
   );
 }
