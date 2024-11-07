81a2d6129c09a5c59a0277c0f78bef8c5448820f
Nguyen Van Toan <toannv@192.168.x.x.non-exists.ptr.local>
2024-11-07 15:00:14 +0700

    Add: User use case

--- cores/domain/lib/src/use_case/user_use_case.dart
+++ cores/domain/lib/src/use_case/user_use_case.dart
@@ -1,3 +1,46 @@
-final class UserUseCase {
+import 'package:domain/core/constant.dart';
+import 'package:domain/core/core.dart';
+import 'package:domain/src/entity/entity.dart';
+import 'package:domain/src/repository/repository.dart';
 
+final class UserUseCase {
+  final UserRepository _userRepository;
+  final SecureLocalStorageRepository _secureLocalStorageRepository;
+
+  const UserUseCase(this._userRepository, this._secureLocalStorageRepository);
+
+  Future<User> getUser() async {
+    final String? accessToken = await ErrorHandler.getInstance().call(
+      request: _secureLocalStorageRepository.get(id: accessTokenKey),
+    );
+
+    return ErrorHandler.getInstance().call(
+      request: _userRepository.get(
+        accessToken: accessToken ?? '',
+      ),
+    );
+  }
+
+  Future<void> update({
+    required String userName,
+    required int gender,
+    required int status,
+  }) async {
+    final String? accessToken = await ErrorHandler.getInstance().call(
+      request: _secureLocalStorageRepository.get(id: accessTokenKey),
+    );
+
+    final UpdateUserRequest updateUserRequest = UpdateUserRequest(
+      name: userName,
+      gender: gender,
+      status: status,
+    );
+
+    await ErrorHandler.getInstance().call(
+      request: _userRepository.update(
+        request: updateUserRequest,
+        accessToken: accessToken ?? '',
+      ),
+    );
+  }
 }
